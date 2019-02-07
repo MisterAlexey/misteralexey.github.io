3aea8543983f9af54aa7c67ef8fca6498fb52421
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2019-02-07 21:58:31 +0700

    Работа с nodeJS

--- 28/index.js
+++ 28/index.js
@@ -11,11 +11,33 @@ function strToInt(arr){
 	return array;
 }
 
+var zero = 0
+var seven = 0
+var before_0 = 0
+var after_7 = 0
+var temp_0 = 0
+
+
 function sorting(arr) {
-	for (var i = 1; i < arr.length; i++) {
-		if (arr[i]==0 ) {
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i]==0) {
+			if (temp_0==0) {
+				before_0 = arr[i-1]
+				temp_0++
+			}
 			arr.splice(i, 1)
+			i--
+			zero++
+			
 		}
+		if (arr[i]==7) {	
+			arr.splice(i, 1)
+			i--
+			seven++
+			after_7 = arr[i+1]
+		}
+	}
+	for (var i = 0; i < arr.length; i++) {
 		var tmp = arr[i]
 		j=i
 		while (j>0 && arr[j-1]>arr[j]){
@@ -31,9 +53,10 @@ function sorting(arr) {
 app.get('/', (req, res) => {
 	fs.readFile('file.txt', 'utf-8', (err, data) =>{
 		var array = data.split(' ')
+
 		array = strToInt(array);
 
-		res.send(JSON.stringify(sorting(array)))
+		res.send(JSON.stringify(sorting(array))+ "Количество нулей - " +zero+" тогда как количество семерок -"+seven+'. Число перед первым нулем - '+before_0+ ', а чисто после последней семерки - '+after_7)
 	})
 })
 app.listen(3000, () => {
