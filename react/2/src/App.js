19cdbb53970cfe5b0a1223bbe43e512efe107260
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2019-03-24 22:01:58 +0700

    создание таблиц в react

--- react/2/src/App.js
+++ react/2/src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
+import json from './json.json'
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users: json
+    }
+    this.delete_card = this.delete_card.bind(this)
+  }
+
+//   componentDidMount(){
+//     fetch(json.json)
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({
+//         users: data
+//       })
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }
+
+  delete_card(e){
+    console.log(e.currentTarget.id)
+    var array = this.state.users;
+    array.splice(e.currentTarget.id, 1);
+    this.setState({
+      users: array
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          this.state.users && this.state.users.map((user, i) => 
+            <Card 
+              first_name={users.first_name}  
+              last_name={users.last_name}
+              delete_card={this.delete_card}
+              id={i}
+            />
+          )
+        }
       </div>
     );
   }
