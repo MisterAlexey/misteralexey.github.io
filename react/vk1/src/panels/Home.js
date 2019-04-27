736e44c31c63397a2985655739c6f91ec7fe33a1
MisterAlexey <44365727+MisterAlexey@users.noreply.github.com>
2019-04-27 16:34:13 +0700

    VK UI

--- react/vk1/src/panels/Home.js
+++ react/vk1/src/panels/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader, List, Cell } from '@vkontakte/vkui';
+import json from '../json.json'
 
 class Home extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {
-
+			burgers: json
 		}
 	}
 	render(){
@@ -19,15 +20,23 @@ class Home extends React.Component{
 						</Cell>
 					</List>
 				</Group>
-				<Group>
-					<List>
-						<Cell before ={<img  src='https://nv.prines.ru/preview.php?src=/upload/images/9-1430286274.png&w=251&h=167&q=80' />}>
-						Длиннобургер
-						<br />
-						<Button onClick={() => this.addItem()}>Добавить один в корзину</Button>
-						</Cell>
-					</List>
-				</Group>
+				{
+					this.props.burgers && this.props.burgers.map((burger,i) => 
+					<Group key={i}>
+						<List>
+							<Cell before ={<img  src={burger.src} />}>
+							{burger.burger_type}
+							<br />
+							{burger.descrition}
+							<br />
+							Цена - {burger.price} рублей
+							<br />
+							<Button id={i} onClick={(e) => this.props.addItem(e)}>Добавить один в корзину</Button>
+							</Cell>
+						</List>
+					</Group>
+					)
+				}
 			</Panel>
 		)
 		
--- react/vk1/src/panels/korzina.js
+++ react/vk1/src/panels/korzina.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader, Textarea, FormLayoutGroup, Checkbox, List, Cell, View, FormLayout, Input, Select, Radio, Link } from '@vkontakte/vkui';
+import json from '../json.json'
 
 class Korzina extends React.Component {
     constructor(props) {
       super(props);
   
       this.state = {
-
+        burgers: json
       }
     }
   
@@ -21,7 +22,30 @@ class Korzina extends React.Component {
       return (
         <Div>
           У вас в корзине:
-          {this.props.json[0].code}
+          {
+            this.state.burgers && this.state.burgers.map((burger, i) => {
+              if(burger.count !== 0){
+                return <Group key={i}>
+                <List>
+                      <Cell before ={<img  src={burger.src} />}>
+                      {burger.burger_type}
+                      <br />
+                        <Button id={i} onClick={(e) => this.props.deleteItem(e)}>Убрать один</Button>
+                        <br />
+                        Количество  - {burger.count}
+                        <br />
+                        Цена  - {burger.price*burger.count}
+                        </Cell>
+                        <Cell>
+                    	
+                </Cell>
+                </List>
+             </Group>
+              }else{
+                return null
+              }
+            })
+          }
         </Div>
           
 
